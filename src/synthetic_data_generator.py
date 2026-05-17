10f933f15c4363658cb2b812d7470b3e39792fbb
Mateja Orlic <mateja.orlic@gmail.com>
2026-05-17 02:18:03 +0200

    Generate synthetic ML dataset with normaln and anemia classes

--- src/synthetic_data_generator.py
+++ src/synthetic_data_generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def generate_normal_patient():
@@ -18,6 +19,37 @@ def generate_normal_patient():
     return patient
 
 
-patient = generate_normal_patient()
+def generate_anemia_patient():
 
-print(patient)
+    patient = {
+        "WBC": round(np.random.normal(7.2, 1.3), 2),
+        "RBC": round(np.random.normal(3.9, 0.4), 2),
+        "HGB": round(np.random.normal(10.2, 1.0), 2),
+        "HCT": round(np.random.normal(32, 3), 2),
+        "PLT": round(np.random.normal(320, 50), 2),
+        "MCV": round(np.random.normal(72, 5), 2),
+        "Neutrophils": round(np.random.normal(57, 5), 2),
+        "Lymphocytes": round(np.random.normal(33, 5), 2),
+        "Class": "Anemia"
+    }
+
+    return patient
+
+
+patients = []
+
+for _ in range(100):
+    patients.append(generate_normal_patient())
+
+for _ in range(100):
+    patients.append(generate_anemia_patient())
+
+df = pd.DataFrame(patients)
+print(df.head())
+
+df.to_csv(
+    "data/raw/blood_analysis_dataset.csv",
+    index=False
+)
+
+print("\nDataset saved successfully.")
